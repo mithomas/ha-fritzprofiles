8fce8f47c06c6da2a8f95313503afcce2be84305
Michael Thomas <mth@michael-thomas.net>
2025-06-09 21:13:13 +0200

    fixing async_add_job deprecation warning

--- custom_components/ha_fritzprofiles/__init__.py
+++ custom_components/ha_fritzprofiles/__init__.py
@@ -56,9 +56,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     for platform in PLATFORMS:
         if entry.options.get(platform, True):
             coordinator.platforms.append(platform)
-            hass.async_add_job(
-                hass.config_entries.async_forward_entry_setups(entry, [platform])
-            )
+            await hass.config_entries.async_forward_entry_setups(entry, [platform])
 
     entry.add_update_listener(async_reload_entry)
     return True
